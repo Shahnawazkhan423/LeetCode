a7f5c4d94073e3d304191374a9d43a409e250350
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-04-18 13:20:14 +0530

    Time: 0 ms (100%), Space: 17.6 MB (95.5%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -1,18 +1,10 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        List1 = []
-        Sub = []
-        start = 0
-        end = 1
-        Sum = 0
-        for i in s:
-            Count= ord(i)
-            List1.append(Count)
-        while start<end and end<len(List1):
-            Subt = List1[start]-List1[end]
-            Sub.append(Subt)
-            start+=1
-            end+=1
-        for i in range(len(Sub)):
-            Sum+=abs(Sub[i])
-        return Sum
+        i = 0
+        j = 1
+        result = 0
+        while j!=len(s):
+            result += abs(ord(s[i]) - ord(s[j]))
+            i+=1
+            j+=1
+        return result
