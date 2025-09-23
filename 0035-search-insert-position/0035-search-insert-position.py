0357a2b9704bcd7db71951f2cd556784ded3bd6a
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-09-23 16:48:37 +0530

    Time: 0 ms (100%), Space: 18.3 MB (93.97%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
+    
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
@@ -11,3 +12,4 @@ class Solution:
                 right = mid - 1
         
         return left
+            
