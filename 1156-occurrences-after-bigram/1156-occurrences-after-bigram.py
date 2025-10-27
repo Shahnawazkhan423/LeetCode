c12466cf03507f4efd30c19a61f5f908c7dcb6fb
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-10-27 14:09:21 +0530

    Time: 0 ms (100%), Space: 17.8 MB (33.54%) - LeetHub

---
+++ 1156-occurrences-after-bigram/1156-occurrences-after-bigram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        Split = text.split()
+        third = []
+        i = 0 
+        j = 1 
+        while i<j and len(Split)>j:
+            if Split[i]==first and Split[j]==second and len(Split)>j+1:
+                third_time = Split[j+1]
+                third.append(third_time)
+            i+=1 
+            j+=1 
+        return third
