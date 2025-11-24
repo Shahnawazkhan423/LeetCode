1b0bb37d702a9a93041f8871d2185b134e5eaa48
Shahnawaz Khan <shahnawazkhan96850@gmail.com>
2025-11-24 14:46:43 +0530

    Time: 4 ms (95.04%), Space: 17.9 MB (81.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,16 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote = "".join(sorted(ransomNote))
-        magazine = "".join(sorted(magazine))
-        i=0
-        j=0 
-        while len(magazine)>j and len(ransomNote)>i:
-            if magazine[j]==ransomNote[i]:
-                i+=1 
-                j+=1
-            else:
-                j+=1 
-        if i==len(ransomNote):
-            return True
-        else:
-            return False
+        Set = set(ransomNote)
+        if len(ransomNote) > len(magazine):
+                return False
+        for i in Set:
+            if ransomNote.count(i)> magazine.count(i):
+                return False
+        
+        return True
